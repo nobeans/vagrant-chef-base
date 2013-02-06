70f28f1e7d76953a40d2d0ce5b88ce4c9e9fa4aa
Yasuharu NAKANO <ynak@jggug.org>
2013-02-06 17:40:17 +0900

    added log_level debug

--- chef-repo/solo.rb
+++ chef-repo/solo.rb
@@ -4,3 +4,4 @@ encrypted_data_bag_secret "/tmp/chef-solo/data_bag_key"
 cookbook_path             [ "/tmp/chef-solo/site-cookbooks",
                             "/tmp/chef-solo/cookbooks" ]
 role_path                 "/tmp/chef-solo/roles"
+log_level :debug
