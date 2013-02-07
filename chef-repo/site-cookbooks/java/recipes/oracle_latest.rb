978f1db99f6fe3dcdb9214cb1894bc4af1aa0877
Yasuharu NAKANO <ynak@jggug.org>
2013-02-07 15:15:16 +0900

    deleted chef-repo directory (because their cookbooks/site-cookbooks have been moved to globally shared chef-repo)

--- chef-repo/site-cookbooks/java/recipes/oracle_latest.rb
+++
@@ -1,7 +0,0 @@
-java_ark "jdk" do
-    url 'http://download.oracle.com/otn-pub/java/jdk/7u13-b20/jdk-7u13-linux-x64.tar.gz'
-    checksum  '5045457407e120207ebd5c5b93ec6ee6ee61d76747ebf3ce467aad51c9c3cb5d'
-    app_home '/usr/local/java/default'
-    bin_cmds ["java", "javac", "jvisualvm", "jar", "keytool", "jstack", "jstat", "jps", "jmap", "jinfo", "jhat", "javap", "javadoc"]
-    action :install
-end
